d8ecbbaa20f59964bc44b4be0f7b419eb19ae278
user <user@user-PC>
2018-08-14 11:08:39 +0900

    MSSQL Setting

--- BootMail/src/main/java/com/mail/main/BootMailApplication.java
+++ BootMail/src/main/java/com/mail/main/BootMailApplication.java
@@ -1,12 +1,25 @@
 package com.mail.main;
 
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BootMailApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootMailApplication.class, args);
+	}
+	
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource);
+		
+		return sessionFactory.getObject();
 	}
 }
